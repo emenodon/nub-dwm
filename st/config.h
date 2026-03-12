b22e4e76bd8d8734d45e4f6b887261a6c7a9f6c8
emen <zamanfw@gmail.com>
2026-03-12 17:00:36 +0700

    feat: add cjk font fallback at st

--- st/config.h
+++ st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Jetbrains Mono NerdFont:pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "Jetbrains Mono NerdFont:pixelsize=15:antialias=true:autohint=true" };
+static char *font = "JetBrains Mono Nerd Font:pixelsize=15:antialias=true:autohint=true";
+static char *font2[] = { "Noto Sans CJK JP:size=12:antialias=true:autohint=true", "Noto Color Emoji:size=12" };
 static int borderpx = 2;
 
 /*
@@ -113,40 +113,40 @@ float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"black",
-        "red3",
-        "green3",
-        "yellow3",
-        "blue2",
-        "magenta3",
-        "cyan3",
-        "gray90",
-	"gray50",
-        "red",
-        "green",
-        "yellow",
-        "#5c5cff",
-        "magenta",
-        "cyan",
-        "white",
-	[255] = 0,
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
-	"#555555", /* 257 -> rev cursor*/
-	"black", /* 258 -> bg */
-	"gray90", /* 259 -> fg */
+	/* 8 normal colors */
+	"#45475A",
+	"#F38BA8",
+	"#A6E3A1",
+	"#F9E2AF",
+	"#89B4FA",
+	"#F5C2E7",
+	"#94E2D5",
+	"#BAC2DE",
+
+	/* 8 bright colors */
+	"#585B70",
+	"#F38BA8",
+	"#A6E3A1",
+	"#F9E2AF",
+	"#89B4FA",
+	"#F5C2E7",
+	"#94E2D5",
+	"#A6ADC8",
+
+	[256] = "#CDD6F4", /* default foreground colour */
+	[257] = "black", /* default background colour */
+	[258] = "#F5E0DC", /*575268*/
+
 };
-
-
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 259;
-unsigned int defaultbg = 258;
-unsigned int defaultcs = 256;
-unsigned int defaultrcs = 257;
-unsigned int background = 258;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+unsigned int defaultrcs = 258;
+unsigned int background = 257;
 
 /*
  * Default shape of cursor
